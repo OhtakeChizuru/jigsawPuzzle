c9bfa24f27bfcf35daeee0cdc10583045d615408
OHTAKE CHIDURU <53688628+OhtakeChizuru@users.noreply.github.com>
2021-12-01 19:14:27 +0800

    リスタートしてタイルをクリックして動くようになった。

--- script/slide_puzzle.js
+++ script/slide_puzzle.js
@@ -44,6 +44,7 @@ function startGame(){
         panel.appendChild(div);
         tiles.push(div);
     }
+    document.addEventListener('click', click)
 
 }
 
@@ -74,20 +75,20 @@ function swapContent(i, k){
 // クリック時の処理
 function click(e) {
     
-    var i = e.target.index;
+    var i = e.target.index
 
     if (i <= 5 && tiles[i + 3].textContent == '' ){
         // 下と入れ替え
-        swapContent(i, i + 3);
+        swapContent(i, i + 3)
     }else if ( i >= 3 && tiles[i - 3].textContent == ''){
         // 上と入れ替え
-        swapContent(i, i - 3);
+        swapContent(i, i - 3)
     }else if (i % 3 !== 2 && tiles[i + 1].textContent == ''){
         // 右と入れ替え
-        swapContent(i, i + 1);
+        swapContent(i, i + 1)
     }else if (i % 3 !== 0 && tiles[i - 1].textContent == ''){
         // 左と入れ替え
-        swapContent(i, i - 1);
+        swapContent(i, i - 1)
     }
 }
 document.addEventListener('click', click)
